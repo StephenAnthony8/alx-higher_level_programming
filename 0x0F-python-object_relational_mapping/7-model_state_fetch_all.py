95eb47c154baab9004cb2ffb8b433a06c2142b92
StephenAnthony8 <anthonysteve02@gmail.com>
2023-12-21 23:25:37 +0300

    q7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,39 +1,17 @@
 #!/usr/bin/python3
-"""model_state_fetch_all script"""
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-from model_state import State, Base
+"""7-model_state_fetch_all
+"""
 import sys
-
-
-def query_state():
-    """Queries the number of states in State table"""
-    if (len(sys.argv) < 4):
-        exit(1)
-
-    host = 'localhost'
-    port = '3306'
-    user = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    db_url = f'mysql+mysqldb://{user}:{password}@{host}:{port}/{database}'
-    engine = create_engine(db_url)
-    Session = sessionmaker(bind=engine)
-
-    Base.metadata.create_all(engine)
-
-    session = Session()
-
-    states = session.query(State).order_by(State.id)
-    for state in states:
-        print(f"{state.id}: {state.name}")
-
-    session.close()
-
-
-def main():
-    query_state()
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    main()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
