ab9fe45db8fee3366b477b173ffff5f48b1a0897
StephenAnthony8 <anthonysteve02@gmail.com>
2024-01-06 05:27:00 +0300

    q1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request to the URL and displays the body of the response
-curl -sX GET "$1" -L 200
+curl -sL -w "%{http_code}" "$1" | grep -z '.*200$' | rev | cut -z -c 4- | rev
