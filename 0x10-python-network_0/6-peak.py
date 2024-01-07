a3c23ba82e6db39893b1db7123e0407b02e32fac
StephenAnthony8 <anthonysteve02@gmail.com>
2024-01-07 04:44:38 +0300

    q6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -14,10 +14,8 @@ def find_peak(list_of_integers):
         mid = (left + right) // 2
 
         if list_of_integers[mid] > list_of_integers[mid + 1]:
-            # If the mid element is greater than its right neighbor, potential peak is on the left
             right = mid
         else:
-            # If the mid element is less than or equal to its right neighbor, potential peak is on the right
             left = mid + 1
 
     return list_of_integers[left]
