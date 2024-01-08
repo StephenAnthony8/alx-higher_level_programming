e0c7f9c983257818524cea42c1dcdfb9bde591aa
StephenAnthony8 <anthonysteve02@gmail.com>
2024-01-08 05:45:58 +0300

    q9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" 4-hbtn_status: fetches site"""
+import requests
+import sys
+
+
+def main():
+    """fetches from 'site"""
+    username, pwd = sys.argv[1:]
+    url = f"http://api.github.com/users/{username}"
+    r = requests.get(url, auth=('username', 'pwd'))
+
+    print(r.json().get('id'))
+
+
+if __name__ == """__main__""":
+    main()
