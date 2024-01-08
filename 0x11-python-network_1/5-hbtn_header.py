a15efcb9d21a27f2b6981f37da5f8a5895f60868
StephenAnthony8 <anthonysteve02@gmail.com>
2024-01-08 04:39:47 +0300

    q5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" 4-hbtn_status: fetches site"""
+import requests
+import sys
+
+
+def main():
+    """fetches from 'site"""
+    site = sys.argv[1]
+    read_content = requests.get(site)
+
+    print(read_content.headers['X-Request-Id'])
+
+
+if __name__ == """__main__""":
+    main()
