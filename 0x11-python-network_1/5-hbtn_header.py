f55f7bd9dbbca5859928440a0c6a6157613cefe3
StephenAnthony8 <anthonysteve02@gmail.com>
2024-01-08 04:45:10 +0300

    q5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -9,7 +9,7 @@ def main():
     site = sys.argv[1]
     read_content = requests.get(site)
 
-    print(read_content.headers['X-Request-Id'])
+    print(read_content.headers.get('X-Request-Id'))
 
 
 if __name__ == """__main__""":
