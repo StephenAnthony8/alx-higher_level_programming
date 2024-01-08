29ef9f0a019e2c6c706b68cd89723bb69012c347
StephenAnthony8 <anthonysteve02@gmail.com>
2024-01-08 03:26:58 +0300

    q2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" 1-hbtn_header: Requests value from URL"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+def main():
+    url, email_addr = sys.argv[1:]
+
+    values = {'email': email_addr}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data=data, method='POST')
+    with urllib.request.urlopen(req) as response:
+        context = response.read().decode('utf-8')
+    print(context)
+
+
+if __name__ == "__main__":
+    main()
