06a2f89ea300e3485d47162880dae2100c175ba3
StephenAnthony8 <anthonysteve02@gmail.com>
2024-02-20 23:01:57 +0300

    I still hate Javascript

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+fs = require("fs");
+
+fs.open(process.argv[2], "w", (err, fd) => {
+	if (err) {
+		console.error(err);
+	}
+	else {
+		fs.write(fd, process.argv[3], (err, bytes) => {
+			if (err) {
+				console.error(err);
+			}
+		})
+	}
+});
