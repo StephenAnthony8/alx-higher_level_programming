4eb9385be7a6f1db255d1e9e398da3ccd47a8c64
StephenAnthony8 <anthonysteve02@gmail.com>
2024-02-20 23:11:08 +0300

    I still hate Javascript

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
 
-fs = require("fs");
+const fs = require('fs');
 
-fs.open(process.argv[2], "w", (err, fd) => {
-	if (err) {
-		console.error(err);
-	}
-	else {
-		fs.write(fd, process.argv[3], (err, bytes) => {
-			if (err) {
-				console.error(err);
-			}
-		})
-	}
+fs.open(process.argv[2], 'w', (err, fd) => {
+  if (err) {
+    console.error(err);
+  } else {
+    fs.write(fd, process.argv[3], (err, bytes) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
 });
