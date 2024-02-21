99325e393c359055d5bf8931a921f147e3be142f
StephenAnthony8 <anthonysteve02@gmail.com>
2024-02-21 05:00:44 +0300

    idk bruv

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,9 +3,7 @@
 const request = require('request');
 
 const urlName = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-const otherUrl = 'https://swapi-api.alx-tools.com/api/people';
 
-let nameList = []
 request({ url: urlName, json: true }, (error, response, body) => {
   if (error || (String(response.statusCode) !== '200')) {
     console.error((error) || response.statusCode);
@@ -16,21 +14,9 @@ request({ url: urlName, json: true }, (error, response, body) => {
         if (error || (String(response.statusCode) !== '200')) {
           console.error((error) || response.statusCode);
         } else {
-          /* nameList.push */console.log(name.name);
+          console.log(name.name);
         }
       });
     }
   }
 });
-
-/* request({ url: otherUrl, json: true }, (error, response, body) => {
-  if (error || (String(response.statusCode) !== '200')) {
-    console.error((error) || response.statusCode);
-  } else {
-    for (people of body.results) {
-      if (nameList.includes(people.name)) {
-        console.log(people.name);
-      }
-    }
-  }
-}) */
